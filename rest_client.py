5587bdf09c3476879895e0f7f15b07487b85650a
padi.litschel@web.de <l\3L$ln[+!5=Lp]7=~e@>
2023-06-19 16:19:35 +0200

    [add] async wait method

--- rest_client.py
+++ rest_client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import threading
 import time
+import asyncio
 
 import paho.mqtt.publish as mqtt_publish
 from flask import Flask
@@ -12,25 +14,56 @@ mqtt_broker = ''
 mqtt_port = 0
 
 
-@app.route('/on/<device>', methods=['GET'])
+@app.route('/<device>/state', methods=['GET'])
+def get_state(device):
+    #TODO
+    mqtt_topic = f'???/{device}/???'
+    mqtt_publish.single(mqtt_topic, payload='???', hostname=mqtt_broker, port=mqtt_port)
+    return f"Ran for {seconds} seconds, then turned OFF"
+
+
+@app.route('/<device>/on', methods=['PUT'])
 def switch_on(device):
     mqtt_topic = f'cmnd/{device}/Power'
     mqtt_publish.single(mqtt_topic, payload='ON', hostname=mqtt_broker, port=mqtt_port)
     return "Switched ON"
 
 
-@app.route('/off/<device>', methods=['GET'])
+@app.route('/<device>/off', methods=['PUT'])
 def switch_off(device):
     mqtt_topic = f'cmnd/{device}/Power'
     mqtt_publish.single(mqtt_topic, payload='OFF', hostname=mqtt_broker, port=mqtt_port)
     return "Switched OFF"
 
 
-@app.route('/run/<device>', methods=['GET'])
-def switch_on_for_duration(device):
+@app.route('/<device>/run', methods=['PUT'])
+def run(device):
     seconds = int(request.args.get('seconds'))
     switch_on(device)
     time.sleep(seconds)
+    switch_off(device)
+    return f"Ran for {seconds} seconds, then turned OFF"
+
+
+@app.route('/<device>/wait', methods=['PUT'])
+def wait(device):
+    seconds = int(request.args.get('seconds'))
+    thread = threading.Thread(target=_run_async_task, args=(seconds, device))
+    thread.start()
+    print("Returning wait")
+    return f"Started running for {seconds} seconds..."
+
+
+def _run_async_task(seconds, device):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(_switch_on_for_duration(seconds, device))
+    loop.close()
+
+
+async def _switch_on_for_duration(seconds, device):
+    switch_on(device)
+    await asyncio.sleep(seconds)
     switch_off(device)
     return f"Ran for {seconds} seconds, then turned OFF"
 
